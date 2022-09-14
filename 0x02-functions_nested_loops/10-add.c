722386d9f9c50059e2577457e965ec3a9e60b72d
Harbby21 <biolaishola4real@gmail.com>
2022-09-14 12:19:30 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,109 +1,13 @@
-#include "main.h"
-
-
+#include "main"
 
 /**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
  *
- *  * print_times_table - prints times table for numbers from 0-14
- *
- *   * @n: An input integer value
- *
- *    * Return: Nothing
- *
- *     */
-
-void print_times_table(int n)
-
+ * Return: The resulty of the addition.
+ */
+int add(int num1, int num2)
 {
-
-		int i, j;
-
-
-
-			if (n > 0 && n < 15)
-
-					{
-
-								for (i = 0; i <= n; i++)
-
-											{
-
-															_putchar('0');
-
-																		for (j = 1; j <= n; j++)
-
-																							putformat(i * j);
-
-																					_putchar('\n');
-
-																							}
-
-									}
-
-}
-
-
-
-/**
- *
- *  * putformat - formatted characters to output
- *
- *   * @n: number to format
- *
- *    * Return: nothing
- *
- *     */
-
-void putformat(int n)
-
-{
-
-		if (n <= 9)
-
-				{
-
-							_putchar(',');
-
-									_putchar(' ');
-
-											_putchar(' ');
-
-													_putchar(' ');
-
-															_putchar(n + '0');
-
-																}
-
-			else if (n > 9 && n <= 99)
-
-					{
-
-								_putchar(',');
-
-										_putchar(' ');
-
-												_putchar(' ');
-
-														_putchar(n / 10 + '0');
-
-																_putchar(n % 10 + '0');
-
-																	}
-
-				else
-
-						{
-
-									_putchar(',');
-
-											_putchar(' ');
-
-													_putchar(n / 100 + '0');
-
-															_putchar(n / 10 % 10 + '0');
-
-																	_putchar(n % 10 + '0');
-
-																		}
-
+	return (num1 + num2);
 }
